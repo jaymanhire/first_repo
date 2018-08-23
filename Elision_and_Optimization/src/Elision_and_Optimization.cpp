4a5c9a2ff9fb9bf22f715514951c14400cab3245
Jay Manhire <jaymanhire@gmail.com>
2018-08-23 18:54:40 -0400

    Constructors and Memory Allocation

--- Elision_and_Optimization/src/Elision_and_Optimization.cpp
+++ Elision_and_Optimization/src/Elision_and_Optimization.cpp
@@ -7,25 +7,46 @@
 //============================================================================
 
 #include <iostream>
+#include <vector>
+#include <memory.h>
 using namespace std;
 
 class Test {
+private:
+	static const int SIZE = 100;
+	int *pBuffer;
 public:
 	Test() {
 		cout << "constructor" << endl;
+		pBuffer = new int[SIZE] { };
+		//memset(pBuffer, 0, sizeof(int)*SIZE);
+
 	}
 	Test(int i) {
 		cout << "parameterized constructor " << endl;
+		pBuffer = new int[SIZE] { };
+
+		for (int i = 0; i < SIZE; i++) {
+			pBuffer[i] = 7 * i;
+		}
 	}
 	Test(const Test &jay) {
-		cout << "parameterized constructor " << endl;
+		cout << "copy constructor " << endl;
+
+		pBuffer = new int[SIZE] { };
+		memcpy(pBuffer, jay.pBuffer, SIZE * sizeof(int));
+
 	}
 	Test &operator=(const Test &jay) {
 		cout << "assignment" << endl;
+		pBuffer = new int[SIZE] { };
+
+		memcpy(pBuffer, jay.pBuffer, SIZE * sizeof(int));
 		return *this;
 	}
 	~Test() {
 		cout << "Destructor" << endl;
+		delete[] pBuffer;
 	}
 
 	friend ostream &operator<<(ostream &out, const Test &test);
@@ -44,6 +65,9 @@ int main() {					//-fno-elide-constructors  /*flag*/
 
 	Test test1 = getTest();
 	cout << test1 << endl;
+
+	vector<Test> vec;
+	vec.push_back(Test());
 
 	return 0;
 }
