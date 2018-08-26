7930cd1812fb062be84677f50ef2d54d83f6e044
Jay Manhire <jaymanhire@gmail.com>
2018-08-25 20:29:08 -0400

    Bak

---
+++ Macros_As_Constants/src/Macros_As_Constants.cpp
@@ -0,0 +1,21 @@
+//============================================================================
+// Name        : Macros_As_Constants.cpp
+// Author      : JLM
+// Version     :
+// Copyright   : 
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+#define ONE_HUNDRED 100
+#define HELLO_STRING "Hello, World!!"
+
+int main() {
+
+	printf("Value is: %d\n", ONE_HUNDRED);
+	printf("String constant: %s\n", HELLO_STRING);
+
+	return 0;
+}
