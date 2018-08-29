8ede0e843f13e00995683ee86baf9cedbfae6396
Jay Manhire <jaymanhire@gmail.com>
2018-08-28 21:30:29 -0400

    Reverse vector output Solved

--- Vectors_Jay/src/Vectors_Jay.cpp
+++ Vectors_Jay/src/Vectors_Jay.cpp
@@ -13,14 +13,15 @@ using namespace std;
 
 int main() {
 
-	vector<string> str_vec = { "Mine", "all", "mein" };
+	vector<string> str_vec = { "first", "second", "third" };
 
 	for (unsigned int i = 0; i < str_vec.size(); ++i) {
 		cout << "Index " << i << ": " << str_vec[i] << endl;
 	}
 	puts("");
 
-	str_vec.push_back("now");
+	str_vec.push_back("fourth");
+	puts("Added index below");
 
 	for (vector<string>::iterator it = str_vec.begin(); it != str_vec.end();
 			it++) {
@@ -28,12 +29,11 @@ int main() {
 		cout << *it << endl;
 	}
 
-	for (vector<string>::iterator bwd = str_vec.end(); bwd != str_vec.begin();
-			bwd--) {
-		cout << *bwd << endl;
-	}
+	puts("--Reverse Below--");
 
-	cout << "Index Position 1: " << str_vec[1] << endl;
+	for (unsigned int j = str_vec.size(); j-- > 0;) {
+		cout << str_vec[j] << endl;
+	}
 
 	return 0;
 }
